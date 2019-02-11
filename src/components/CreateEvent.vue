935d2635e63dd623bb2dae18b9d6467ae5b446c5
ingmar <ingmar@liibert.ee>
2019-02-11 15:54:58 +0200

    #2 Created CreateEvent.vue; added Bootstrap to project

---
+++ src/components/CreateEvent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row">
+        <input type="text">Title
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CreateEvent"
+    }
+</script>
+
+<style scoped>
+
+</style>
