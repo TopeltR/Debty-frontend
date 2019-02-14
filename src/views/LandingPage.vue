ccdd4a4ccce1df07617f34b2c4b4b2b5fe58ae8a
r.ryngenen@gmail.com <Maarika5>
2019-02-14 11:08:13 +0200

    Updated Landing page :snowflake:

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,14 +1,14 @@
 <template>
-    <b-row>
-        <b-col>
+    <b-row class="customRow">
+        <b-col class="columns">
            Make the Logo AS BIG AS YOU CAN
         </b-col>
-        <b-col>
-            <b-jumbotron>
+        <b-col class="columns" >
+            <b-jumbotron class="jumbo" lead="Login or sign up or idk die" >
                 <b-container class="text-center" >
-                    <b-button-group vertical id="mainButtons">
-                        <b-btn variant="primary" href="#">Login</b-btn>
-                        <b-btn variant="success" href="#">Sign up</b-btn>
+                    <b-button-group vertical>
+                        <b-btn variant="primary" href="#" id="loginButton">Login</b-btn>
+                        <b-btn variant="success" href="#" id="signupButton">Sign up</b-btn>
                     </b-button-group>
                 </b-container>
             </b-jumbotron>
@@ -23,7 +23,27 @@
 </script>
 
 <style scoped>
-    #mainButtons {
+    .jumbo {
+        height:65vh;
+    }
+    .customRow {
+        padding:10vh;
+    }
+    #loginButton {
+        margin-bottom: 20px;
+        width: 10vw;
+        height: 10vh;
+    }
+    #signupButton {
+        width: 10vw;
+        height: 10vh;
+    }
+
+    .columns{
+        height: 90vh;
+    }
+
+    #loginButton {
         margin-top: 20px;
     }
 </style>
