1c7f7976f776378dc060911bce71bb766c6505e7
Liine Kasak <liine.kasak@gmail.com>
2019-02-16 17:44:47 +0200

    some shit front page design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
+
     <b-container>
         <router-view/>
     </b-container>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+    }
 </style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,19 +1,36 @@
 <template>
-    <b-row class="customRow">
-        <b-col class="columns">
-           Make the Logo AS BIG AS YOU CAN
-        </b-col>
-        <b-col class="columns" >
-            <b-jumbotron class="jumbo" lead="Login or sign up or idk die" >
-                <b-container class="text-center" >
-                    <b-button-group vertical>
-                        <b-btn variant="primary" href="#" id="loginButton">Login</b-btn>
-                        <b-btn variant="success" href="#" id="signupButton">Sign up</b-btn>
-                    </b-button-group>
-                </b-container>
-            </b-jumbotron>
-        </b-col>
-    </b-row>
+        <b-row class="MT20vh">
+            <div class="diagonal-background"></div>
+            <b-col sm="12" md="6">
+                Make the Logo AS BIG AS YOU CAN
+            </b-col>
+            <b-col sm="12" md="5" offset-md="1">
+                <form @submit.prevent="login">
+                    <div class="form-group">
+                        <b-row>
+                            <b-col>
+                                <label for="email">Email</label>
+                                <input type="text" class="form-control" id="email" placeholder="Email" v-model="email">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div class="form-group">
+                        <b-row>
+                            <b-col>
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" placeholder="Password"
+                                       v-model="password">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <b-row>
+                        <b-col sm="12" md="12">
+                            <b-btn variant="primary" type="submit" class="button-wide">Login</b-btn>
+                        </b-col>
+                    </b-row>
+                </form>
+            </b-col>
+        </b-row>
 </template>
 
 <script>
@@ -23,27 +40,18 @@
 </script>
 
 <style scoped>
-    .jumbo {
-        height:65vh;
-    }
-    .customRow {
-        padding:10vh;
-    }
-    #loginButton {
-        margin-bottom: 20px;
-        width: 10vw;
-        height: 10vh;
-    }
-    #signupButton {
-        width: 10vw;
-        height: 10vh;
+    .MT20vh {
+        margin-top: 25vh;
     }
 
-    .columns{
-        height: 90vh;
+    .button-wide {
+        width: 100%;
     }
 
-    #loginButton {
-        margin-top: 20px;
+    .diagonal-background {
+        background-size: cover;
+        height: 100vh;
+        width: 100vw;
+        background: url("../assets/bg-diagonal.svg") no-repeat 0 0;
     }
 </style>
