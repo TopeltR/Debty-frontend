72882eba7f5afae128ffb8d741953b3264ec19c8
Liine Kasak <liine.kasak@gmail.com>
2019-02-16 20:55:27 +0200

    just a commit and push - actually for background.vue

---
+++ src/components/Background.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-container fluid class="diagonal-background">
+        <b-container>
+            <slot></slot>
+        </b-container>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "Background"
+    }
+</script>
+
+<style scoped>
+    .diagonal-background {
+        background: url("../assets/bg-diagonal.svg") no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+        height: 100vh;
+    }
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,58 +1,59 @@
 <template>
-    <b-container fluid class="diagonal-background">
-        <b-container>
-            <b-row class="PT15vh">
-                <b-col sm="12" md="6">
-                    <h1>LOGO</h1>
-                </b-col>
-                <b-col sm="12" md="5" offset-md="1">
-                    <form @submit.prevent="login">
-                        <b-row v-bind:style="{display: displayProperty}">
-                            <b-col sm="12">
-                                <div class="notification">
-                                    <b-row>
-                                        <b-col sm="10">
-                                            <h6>Login failed! Wrong email or password</h6>
-                                        </b-col>
-                                    </b-row>
-                                </div>
-                            </b-col>
-                        </b-row>
-                        <div class="form-group">
-                            <b-row>
-                                <b-col>
-                                    <label for="email">Email</label>
-                                    <input type="text" class="form-control" id="email" placeholder="Email"
-                                           v-model="email">
-                                </b-col>
-                            </b-row>
-                        </div>
-                        <div class="form-group">
-                            <b-row>
-                                <b-col>
-                                    <label for="password">Password</label>
-                                    <input type="password" class="form-control" id="password" placeholder="Password"
-                                           v-model="password">
-                                </b-col>
-                            </b-row>
-                        </div>
+    <background>
+        <b-row class="PT15vh">
+            <b-col sm="12" md="6">
+                <h1>LOGO</h1>
+            </b-col>
+            <b-col sm="12" md="5" offset-md="1">
+                <form @submit.prevent="login">
+                    <b-row v-bind:style="{display: displayProperty}">
+                        <b-col sm="12">
+                            <div class="notification">
+                                <b-row>
+                                    <b-col sm="10">
+                                        <h6>Login failed! Wrong email or password</h6>
+                                    </b-col>
+                                </b-row>
+                            </div>
+                        </b-col>
+                    </b-row>
+                    <div class="form-group">
                         <b-row>
-                            <b-col sm="12" md="12">
-                                <b-btn variant="primary" type="submit" class="button-wide">Login</b-btn>
+                            <b-col>
+                                <label for="email">Email</label>
+                                <input type="text" class="form-control" id="email" placeholder="Email"
+                                       v-model="email">
                             </b-col>
                         </b-row>
-                    </form>
-                </b-col>
-            </b-row>
-        </b-container>
-    </b-container>
+                    </div>
+                    <div class="form-group">
+                        <b-row>
+                            <b-col>
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" placeholder="Password"
+                                       v-model="password">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <b-row>
+                        <b-col sm="12" md="12">
+                            <b-btn variant="primary" type="submit" class="button-wide">Login</b-btn>
+                        </b-col>
+                    </b-row>
+                    <a href="/#/register">Register</a>
+                </form>
+            </b-col>
+        </b-row>
+    </background>
 </template>
 
 <script>
     import router from '../router.ts';
+    import Background from "../components/Background";
 
     export default {
         name: 'LandingPage',
+        components: {Background},
         data: () => ({
             email: '',
             password: '',
@@ -71,8 +72,8 @@
                             router.push('/home')
                         }
                     }).catch(error => {
-                        this.displayProperty = 'block'
-                    });
+                    this.displayProperty = 'block'
+                });
             },
         },
     };
@@ -82,27 +83,14 @@
     .PT15vh {
         padding-top: 15vh;
     }
-
     @media (max-width: 767px) {
         .PT15vh {
             padding-top: 0;
         }
     }
-
-
     .button-wide {
         width: 100%;
     }
-
-    .diagonal-background {
-        background: url("../assets/bg-diagonal.svg") no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-        height: 100vh;
-    }
-
     .notification {
         height: 45px;
         color: red;
