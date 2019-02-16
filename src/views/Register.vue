d4c40ad6951c47b831b552d642249db88a09eb06
Liine Kasak <liine.kasak@gmail.com>
2019-02-16 21:11:34 +0200

    register design - stuff doesnt work tho :sad:

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,130 @@
 <template>
-    
+    <background>
+        <b-row class="PT15vh">
+            <b-col sm="12" md="6">
+                <p>
+                    <b>
+                        Doggo ipsum bork heckin angery woofer borking doggo wrinkler vvv shooberino pupper, ur givin me
+                        a spook smol pupperino long water shoob.
+                    </b>
+                    <br><br>
+                    Sub woofer big ol pupper such treat heckin good boys he made many woofs much ruin diet very good
+                    spot, puggorino super chub doggorino such treat. Doge you are doing me the shock wrinkler,
+                    borkdrive. Many pats boof heckin good boys and girls heckin the neighborhood pupper waggy wags
+                    shibe, noodle horse aqua doggo you are doing me a frighten shibe fluffer. Shoober sub woofer much
+                    ruin diet noodle horse pupper, stop it fren dat tungg tho.
+                </p>
+            </b-col>
+            <b-col sm="12" md="5" offset-md="1">
+                <form @submit.prevent="register">
+                    <div class="form-group text-right">
+                        <b-row>
+                            <b-col>
+                                <input type="text" class="form-control" id="email" placeholder="Email"
+                                       v-model="email">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div class="form-group text-right">
+                        <b-row>
+                            <b-col>
+                                <input type="password" class="form-control" id="password" placeholder="Password"
+                                       v-model="password">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div class="form-group text-right">
+                        <b-row>
+                            <b-col>
+                                <input type="password" class="form-control" id="passwordConfirm" placeholder="Repeat password"
+                                       v-model="passwordConfirm">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div class="form-group text-right">
+                        <b-row>
+                            <b-col>
+                                <input type="text" class="form-control" id="firstName" placeholder="First name"
+                                       v-model="firstName">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div class="form-group text-right">
+                        <b-row>
+                            <b-col>
+                                <input type="text" class="form-control" id="lastName" placeholder="Last name"
+                                       v-model="lastName">
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <b-row>
+                        <b-col sm="12" md="5">
+                            <b-btn variant="primary" type="submit" class="button-wide">Register</b-btn>
+                        </b-col>
+                        <b-col sm="12" md="7" class="MT7">
+                            <a href="#">Already a member?</a>
+                        </b-col>
+                    </b-row>
+                </form>
+            </b-col>
+        </b-row>
+    </background>
 </template>
 
 <script>
+    import router from '../router.ts';
+    import Background from "../components/Background";
+    import {required, email, minLength} from "vuelidate/lib/validators";
+
     export default {
-        name: "Register"
+        name: "Register",
+        components: {Background},
+        validations: {
+            form: {
+                email: {required, email},
+                password: {required, min: minLength(8)},
+            }
+        },
+        data: () => ({
+            email: '',
+            password: '',
+            passwordConfirm: '',
+            firstName: '',
+            lastName: ''
+        }),
+        methods: {
+            login() {
+
+                this.$http.post('/login',
+                    {},
+                    {
+                        headers: {'Content-Type': 'multipart/form-data'}
+                    })
+                    .then(response => {
+                        if (response.status === 200) {
+                            router.push('/home')
+                        }
+                    }).catch(error => {
+                    this.displayProperty = 'block'
+                });
+            },
+        },
     }
 </script>
 
 <style scoped>
-
+    .PT15vh {
+        padding-top: 15vh;
+    }
+    .MT7 {
+        margin-top: 7px;
+    }
+    @media (max-width: 767px) {
+        .PT15vh {
+            padding-top: 0;
+        }
+    }
+    .button-wide {
+        width: 100%;
+    }
 </style>
