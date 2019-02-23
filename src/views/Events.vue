fae21f3a66f2487d5909d2b9211ad841dc3e8398
r.ryngenen@gmail.com <Maarika5>
2019-02-23 16:17:10 +0200

    Created Events view, table does not work. Need to create a way to put items into table.

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -3,7 +3,7 @@
         <navbar></navbar>
         <background>
             <h1 class="header">My events</h1>
-            <b-table hover :busy.sync="isBusy" :fields="fields" :items="getEvents" ></b-table>
+            <b-table class="events" hover :busy.sync="isBusy" :fields="fields" :items="getEvents" sortable="true" ></b-table>
         </background>
     </div>
 </template>
@@ -12,6 +12,12 @@
     import Navbar from '@/components/Navbar.vue';
     import Background from '@/components/Background.vue';
     import router from '@/router';
+    import axios from 'axios';
+
+    /**
+     * Documentation can be found https://bootstrap-vue.js.org/docs/components/table/#event-based-refreshing-of-data
+     *
+     */
 
     export default {
         name: 'events',
@@ -22,26 +28,43 @@
         },
         data() {
             return {
-                fields: ['Title', 'Owner', 'Created at', 'Modified at'],
+                fields: [
+                    { key: 'title', label: 'Title' },
+                    { key: 'owner.firstName', label: 'Owner Firstname' },
+                    { key: 'owner.lastName', label: 'Owner lastname' },
+                    { key: 'created', label: 'Created' },
+                    { key: 'modified', label: 'Modified' },
+                    { key: 'people', label: 'People' },
+                    { key: 'bills', label: 'Bills' },
+                ],
                 isBusy: false
             }
         },
         methods: {
-            getEvents(ctx) {
-                this.$http.get('/events/all').then(
-                    (response) => {
-                        let items = [];
-                        items = response.data;
-                        this.isBusy = false;
-                        return items
-                    },
-                ).catch(
-                    (error) => {
-                        alert('You are not logged in!');
-                        router.push('/');
-                        return []
-                    },
-                );
+            getEvents() {
+                /*   this.$http.get('/events/all').then(
+                   (response) => {
+                       let items = [];
+                       items = response.data;
+                       console.log(items);
+                       this.isBusy = false;
+                       return items
+                   },
+               ).catch(
+                   (error) => {
+                       alert('You are not logged in!');
+                       router.push('/');
+                       return []
+                   },
+               );*/
+
+                let promise = axios.get('http://localhost:8080/events/all');
+                return promise.then(data => {
+                    let items = data.data;
+                    return items || [];
+                })
+
+
             },
         },
     }
@@ -50,5 +73,8 @@
 <style>
     .header {
         padding-top: 30px
+    }
+    .events {
+        padding-top: 600px !important;
     }
 </style>
