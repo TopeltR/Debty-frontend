a4921687fe0e60e811f4964e1d82681a39877433
r.ryngenen@gmail.com <Maarika5>
2019-02-23 17:58:33 +0200

    Fixed api call url

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -3,7 +3,7 @@
         <navbar></navbar>
         <background>
             <h1 class="header">My events</h1>
-            <b-table hover :busy.sync="isBusy" :fields="fields" :items="getEvents"></b-table>
+            <b-table class="events" hover :busy.sync="isBusy" :fields="fields" :items="items" sortable="true" ></b-table>
         </background>
     </div>
 </template>
@@ -12,6 +12,12 @@
     import Navbar from '@/components/Navbar.vue';
     import Background from '@/components/Background.vue';
     import router from '@/router';
+    import axios from 'axios';
+    import userStore from "../stores/UserStore";
+    /**
+     * Documentation can be found https://bootstrap-vue.js.org/docs/components/table/#event-based-refreshing-of-data
+     *
+     */
 
     export default {
         name: 'events',
@@ -22,37 +28,32 @@
         },
         data() {
             return {
+                items: [],
                 fields: [
-                    { key: 'title', label: 'Title' },
-                    { key: 'owner.firstName', label: 'Owner Firstname' },
-                    { key: 'owner.lastName', label: 'Owner lastname' },
-                    { key: 'created', label: 'Created' },
-                    { key: 'modified', label: 'Modified' },
-                    { key: 'people', label: 'People' },
-                    { key: 'bills', label: 'Bills' },
+                    { key: 'title', label: 'Title', sortable:true },
+                    { key: 'owner.firstName', label: 'Owner Firstname', sortable:true },
+                    { key: 'owner.lastName', label: 'Owner lastname', sortable:true },
+                    { key: 'created', label: 'Created', sortable:true },
+                    { key: 'modified', label: 'Modified', sortable:true },
+                    { key: 'people.length', label: 'People', sortable:true },
                 ],
                 isBusy: false
             }
         },
         methods: {
             getEvents() {
-                this.$http.get('/events/all').then(
-                    (response) => {
-                        let items = [];
-                        items = response.data;
-                        this.isBusy = false;
-                        return items;
-                    },
-                ).catch(
-                    (error) => {
-                        alert('You are not logged in!');
-                        router.push('/');
-                        return [];
-                    },
-                );
+                let self = this;
+                userStore.getUser().then(user => {
+                    console.log(user.id);
+                    let promise = axios.get('http://localhost:8080/events/user/' + user.id);
+                    promise.then(data => {
+                        self.items = data.data;
+                        console.log(self.items);
+                    })
+                });
             },
         },
-    };
+    }
 </script>
 
 <style>
